c9ec184f44a06c5ed0293fcea569b0af5d07e46f
Dear0001 <dalenphea@gmail.com>
2024-05-04 00:47:25 +0700

    all component

--- src/components/BannerText.jsx
+++ src/components/BannerText.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BannerText = () => {
     return (
         <div className={"absolute z-10"}>
-            <h1>Let's us Find You Room!</h1>
+            <h1>Let&apos;s us Find You Room!</h1>
             <p>Find your most reliable and reasonable room at ease or  gain more renters</p>
         </div>
     );
