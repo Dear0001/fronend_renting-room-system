ee624c53ccd2aa38d2cb34d056fc87e4c20a9f99
Dear0001 <dalenphea@gmail.com>
2024-05-26 02:19:29 +0700

    hello

--- src/components/ImageScroll.jsx
+++ src/components/ImageScroll.jsx
@@ -4,7 +4,7 @@ import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import Image from "next/image";
 import { data } from './mockData';
 
-const ImageScroll = ({image}) => {
+const ImageScroll = () => {
     const sliderRef = useRef(null);
 
     const slideLeft = () => {
@@ -26,8 +26,8 @@ const ImageScroll = ({image}) => {
                     id='slider'
                     className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'
                 >
-                    {data.map((item, index) => (
-                        <div className='inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
+                    {data.map((item) => (
+                        <div key={item.id} className='inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
                             <Image
                                 src={item.img}
                                 alt='/'
