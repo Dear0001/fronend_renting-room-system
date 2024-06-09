7c61d3ad48fcb8cb6e224b05369f9e868b0f2558
Dear0001 <dalenphea@gmail.com>
2024-06-09 16:32:31 +0700

    login with gg

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -13,6 +13,14 @@ export const authOptions  = {
             clientSecret: process.env.FRESHBOOKS_CLIENT_SECRET,
         })
     ],
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account.provider === "google") {
+                return profile.email_verified && profile.email.endsWith("@example.com")
+            }
+            return true
+        },
+    }
 };
 
 const handler = NextAuth(authOptions);
